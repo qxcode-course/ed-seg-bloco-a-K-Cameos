71fc774d5cc820cf0c837c32d847754bbebed0f4
Cameos <kauacameos@gmail.com>
2026-04-13 22:30:51 +0000

    arc, apa

--- tko/eval1/apagando/src/go/draft.go
+++ tko/eval1/apagando/src/go/draft.go
@@ -1,4 +1,29 @@
 package main
 import "fmt"
 func main() {
+	var n int
+	fmt.Scan(&n)
+
+	fila := make([]string, n)
+	for i := range fila {
+		fmt.Scan(&fila[i])
+	}
+
+	var m int
+	fmt.Scan(&m)
+
+	fora := make (map[string]struct{}, m)
+	for i := 0; i < m;i++ {
+		var num string
+		fmt.Scan(&num)
+		fora[num] = struct{}{}
+	}
+
+	for _, num := range fila {
+		if _, ok := fora[num]; !ok {
+			fmt.Print(num, " ")
+		}
+	}
+	fmt.Println()
+
 }
